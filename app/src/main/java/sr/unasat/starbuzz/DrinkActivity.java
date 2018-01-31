82b98c4aa23bed8b487508bb11b83f1c912a738d
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2018-01-31 20:21:22 -0300

    Chapter 16 Done

--- app/src/main/java/sr/unasat/starbuzz/DrinkActivity.java
+++ app/src/main/java/sr/unasat/starbuzz/DrinkActivity.java
@@ -1,9 +1,14 @@
 package sr.unasat.starbuzz;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -17,16 +22,36 @@ public class DrinkActivity extends Activity {
         setContentView(R.layout.activity_drink);
 
         int drinkId = (Integer)getIntent().getExtras().get(EXTRA_DRINKID);
-        Drink drink = Drink.drinks[drinkId];
+//        Drink drink = Drink.drinks[drinkId]; -> no longer needed
 
-        TextView name = (TextView)findViewById(R.id.name);
-        name.setText(drink.getName());
 
-        TextView description = (TextView)findViewById(R.id.description);
-        description.setText(drink.getDescription());
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try{
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query("DRINK", new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"}, "_id = ?", new String[] {Integer.toString(drinkId)}, null, null, null);
 
-        ImageView photo = (ImageView)findViewById(R.id.photo);
-        photo.setImageResource(drink.getImageResourceId());
-        photo.setContentDescription(drink.getName());
+            if(cursor.moveToFirst()){
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
+
+                TextView name = (TextView)findViewById(R.id.name);
+                name.setText(nameText);
+
+                TextView description = (TextView)findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                ImageView photo = (ImageView)findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+
+                // close the cursor and database
+                cursor.close();
+                db.close();
+            }
+        } catch(SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
