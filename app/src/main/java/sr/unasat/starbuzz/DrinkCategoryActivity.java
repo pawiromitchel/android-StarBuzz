fb093f419e6a8a88c3cdc9f97c67722bbe354dfb
Mitchel Pawirodinomo <pawiromitchel@hotmail.com>
2018-01-31 18:59:42 -0300

    Chapter 7 Done

---
+++ app/src/main/java/sr/unasat/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,33 @@
+package sr.unasat.starbuzz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DrinkCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Drink.drinks);
+        ListView listDrinks = (ListView) findViewById(R.id.listDrinks);
+        listDrinks.setAdapter(listAdapter);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) l);
+                startActivity(intent);
+            }
+        };
+
+        listDrinks.setOnItemClickListener(itemClickListener);
+    }
+}
